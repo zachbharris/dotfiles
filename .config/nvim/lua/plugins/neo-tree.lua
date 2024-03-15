dfc59bd2272b2e14b9848d7962b347bef61fa95c
zachbharris <zachbharris94@gmail.com>
2024-03-15 18:05:19 -0500

    init

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+		})
+	end,
+}
